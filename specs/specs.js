640dde3e780271750ff1815df7d6091c9c88aec8
HeyConner <c9er99@gmail.com>
2016-01-31 21:50:26 -0800

    changed wording

--- specs/specs.js
+++
@@ -1,6 +0,0 @@
-describe('pizzaToppings', function(pizza) {
-  it("is is true for the user inputting any of the toppings available", function() {
-    expect(pizzaToppings(jalapeno)).to.equal(true);
-  });
-  it("is false for toppings undefined in pizzaTopping")
-})
